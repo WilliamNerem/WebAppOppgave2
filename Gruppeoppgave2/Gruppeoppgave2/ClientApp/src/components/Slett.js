9fc11150cbd168e774d8336d36ce2bff0c892bac
mbrandal <brandalmartin@gmail.com>
2021-10-18 19:34:46 +0200

    Lagt til 2x komponenter

---
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/components/Slett.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import $ from 'jquery';
+
+export function Slett() {
+
+    function slettStrekning(id) {
+        $.get(url, function (OK) {
+            if (OK) {
+                window.location.href = 'index.html';
+            }
+            else {
+                $("#feil").html("Feil i db - vennligst forsøk igjen");
+            }
+        });
+    }
+}
