94f508516a6ca183b7cfbfac691189b3860ac46b
WilliamNerem <william-kn@hotmail.com>
2021-10-18 18:54:17 +0200

    lagt til hentalle komponent

--- Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/Pages/Departures.js
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/Pages/Departures.js
@@ -1,37 +1,10 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
 import $ from 'jquery';
-let ut = "";
+import { HentAlle } from '../components/HentAlle';
 
 export function Departures() {
 
-    function hentAlleStrekninger() {
-        $.getJSON("strekning/hentAlle", function (strekninger) {
-            formaterStrekninger(strekninger);
-            console.log(strekninger)
-        });
-    }
-
-    function formaterStrekninger(strekninger) {
-        ut += " <table className='table'>" +
-            "< thead >" +
-            "<tr>" +
-            "<th scope='col'>Strekning:</th>" +
-            "<th scope='col'>Pris:</th>" +
-            "</tr>" +
-            "</thead >" +
-            "<body>";
-        for (let strekning of strekninger) {
-            ut += "<tr>" +
-                "<td>" + strekning.navn + "</td>" +
-                "<td>" + strekning.pris + "</td>" +
-                "</tr>";
-        }
-        ut += "</body>" +
-            "</table >";
-    }
-    hentAlleStrekninger();
-    console.log(ut)
     return (
-        <div id="strekningene"> {ut} </div>
-    );
+        <HentAlle/>
+        );
 }
---
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/components/HentAlle.js
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+import $ from 'jquery';
+
+export function HentAlle() {
+
+    function hentAlleStrekninger() {
+        $.getJSON("strekning/hentAlle", function (strekninger) {
+            formaterStrekninger(strekninger);
+        });
+    }
+
+    function formaterStrekninger(strekninger) {
+        let ut = " <table className='table'>" +
+            "<thead>" +
+            "<tr>" +
+            "<th scope='col'>Strekning:</th>" +
+            "<th scope='col'>Pris:</th>" +
+            "</tr>" +
+            "</thead >" +
+            "<body>";
+        for (let strekning of strekninger) {
+            ut += "<tr>" +
+                "<td>" + strekning.navn + "</td>" +
+                "<td>" + strekning.pris + "</td>" +
+                "</tr>";
+        }
+        ut += "</body>" +
+            "</table >";
+        document.getElementById("strekningene").innerHTML = ut;
+    }
+
+    hentAlleStrekninger();
+
+    return (
+        <div id="strekningene"></div>
+    );
+}
