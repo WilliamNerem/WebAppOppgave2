84ded7f9c797e36fdda89218c3f653db5087aea5
Maren <maren.lund@wemail.no>
2021-10-18 18:30:01 +0200

    idk ass dude

--- Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/Pages/Departures.js
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/Pages/Departures.js
@@ -29,9 +29,43 @@ export function Departures() {
         ut += "</body>" +
             "</table >";
     }
+
     hentAlleStrekninger();
+
     console.log(ut)
+
     return (
+
         <div id="strekningene"> {ut} </div>
+
     );
+
 }
+
+// -- DETTE ER DEFAULT STYLE FOR TABLE, IKKE SLETT!! --
+    //return (
+    //    <div>
+    //        <table className='table'>
+    //            <thead>
+    //                <tr className='table-primary'>
+    //                    <th scope="col">#</th>
+    //                    <th scope='col'>Strekning:</th>
+    //                    <th scope='col'>Pris:</th>
+    //                </tr>
+    //            </thead>
+    //            <tbody>
+    //                <tr>
+    //                    <th scope="row">1</th>
+    //                    <td>Oslo-Kiel</td>
+    //                    <td>1999 kr</td>
+    //                </tr>
+    //                <tr>
+    //                    <th scope="row">2</th>
+    //                    <td>Kiel-Oslo</td>
+    //                    <td>1999 kr</td>
+    //                </tr>
+    //            </tbody>
+    //        </table>
+    //    </div>
+    //);
+//};
