2d79bd2a51153bfad53ae41d19377a01f9a4a7dc
mbrandal <brandalmartin@gmail.com>
2021-11-01 20:44:34 +0100

    Lagt til logging til fil

--- Gruppeoppgave2/Gruppeoppgave2/Startup.cs
+++ Gruppeoppgave2/Gruppeoppgave2/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Gruppeoppgave2
@@ -44,11 +45,12 @@ namespace Gruppeoppgave2
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                loggerFactory.AddFile("Logs/StrekningLog.txt");
                 DBInit.init(app);
             }
             else
