fac6749d6dfa1f5c1c9319721699bba3c6baca7a
WilliamNerem <william-kn@hotmail.com>
2021-11-02 10:58:46 +0100

    Lagt til validering på innlogging

--- Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/Pages/Home.js
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/Pages/Home.js
@@ -1,46 +1,10 @@
-import React, { useState } from 'react';
-import { InputField } from '../components/InputField';
-import { Button } from '../components/Button';
-import $ from 'jquery';
+import React from 'react';
+import { Login } from '../components/Login';
 
 
 
 export function Home() {
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
-
-    function handleSubmit(e) {
-        if ((username !== "admin") || (password !== "admin")) {
-            e.preventDefault();
-        }
-    }
-
-    function login(e) {
-        const admin = {
-            brukernavn: username,
-            passord: password
-        };
-        console.log(admin.brukernavn);
-        console.log(admin.passord);
-        $.post("Strekning/Logginn", admin, function (OK) {
-            if (OK) {
-                window.location.href = '/departures'
-            }
-            else {
-                e.preventDefault();
-            }
-        });
-        
-    }
-    function stoplogin(e) {
-        e.preventDefault();
-    }
-
     return (
-        <form onSubmit={login}>
-            <InputField state={setUsername} name="Brukernavn" InputField="Brukernavn: " />
-            <InputField state={setPassword} name="Passord" InputField="Passord: " />
-            <Button text="Logg Inn" />
-        </form>
+        <Login />
     );
 }
---
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/components/Login.js
@@ -0,0 +1,44 @@
+﻿import React, { useState } from 'react';
+import { InputField } from '../components/InputField';
+import { Button } from '../components/Button';
+import $ from 'jquery';
+
+
+
+export function Login() {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState("");
+
+    function login(e) {
+        const admin = {
+            brukernavn: username,
+            passord: password
+        };
+        
+        $.post("Strekning/Logginn", admin, function (OK) {
+            if (OK) {
+                window.location.href = '/departures'
+                setError("");
+            }
+            else {
+                setError("Feil brukernavn eller passord");
+                return false;
+            }
+        });
+
+    }
+    function checklogin(e) {
+        if (!login()) {
+            e.preventDefault();
+        }
+    }
+
+    return (
+        <form onSubmit={checklogin}>
+            <InputField state={setUsername} name="Brukernavn" InputField="Brukernavn: " />
+            <InputField errorMsg={error} state={setPassword} name="Passord" InputField="Passord: " />
+            <Button text="Logg Inn" />
+        </form>
+    );
+}
