bd825ed575674c6c60e2768135daaed768c263b1
mbrandal <brandalmartin@gmail.com>
2021-11-04 18:54:33 +0100

    Flere kommentarer

--- Gruppeoppgave2/Gruppeoppgave2/Controllers/StrekningController.cs
+++ Gruppeoppgave2/Gruppeoppgave2/Controllers/StrekningController.cs
@@ -3,9 +3,7 @@ using Gruppeoppgave2.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Gruppeoppgave2.Controllers
--- Gruppeoppgave2/Gruppeoppgave2/DAL/IStrekningRepository.cs
+++ Gruppeoppgave2/Gruppeoppgave2/DAL/IStrekningRepository.cs
@@ -1,7 +1,5 @@
 ﻿using Gruppeoppgave2.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Gruppeoppgave2.DAL
