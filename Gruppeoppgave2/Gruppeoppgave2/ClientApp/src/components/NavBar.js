389246015415dcdbbecaf2766b50f86e842d74db
Maren <maren.lund@wemail.no>
2021-11-04 17:19:59 +0100

    fixed bg img

--- Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/components/NavBar.js
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/components/NavBar.js
@@ -31,30 +31,32 @@ export function NavBar() {
     }
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-                <a className="navbar-brand bg-light" href="/departures">
-                    <img src={img} alt="Image of Color Line Logo" width="100" height="50" />
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active text-light" aria-current="page" href="/departures">Hjem</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active text-light" aria-current="page" href="/departures">Strekninger</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active text-light">{loggedIn}</a>
-                        </li>
-                        {logOutButton}
-                    </ul>
+        <>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand bg-light" href="/departures">
+                        <img src={img} alt="Image of Color Line Logo" width="100" height="50" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active text-light" aria-current="page" href="/departures">Hjem</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active text-light" aria-current="page" href="/departures">Strekninger</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active text-light">{loggedIn}</a>
+                            </li>
+                            {logOutButton}
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </nav>
             <Modal id={modalId} continue={logOut} title={modalTitle} body={modalBodyText} dismissBtn={modalDismissText} continueBtn={modalContinueText} />
-        </nav>
+        </>
     );
 }
