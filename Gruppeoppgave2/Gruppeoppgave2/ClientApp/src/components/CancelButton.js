849cc064954b33aefd7495bdaf3b320a33604cbd
WilliamNerem <william-kn@hotmail.com>
2021-11-04 20:38:14 +0100

    Lagt til CancelButton IGJEN

---
+++ Gruppeoppgave2/Gruppeoppgave2/ClientApp/src/components/CancelButton.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+export function CancelButton(props) {
+
+    function handleClick() {
+        window.location.href = '/departures';
+    }
+
+    return (
+        <button onclick={handleClick} className="btn btn-danger cancelBtn">Avbryt</button>
+    );
+}
